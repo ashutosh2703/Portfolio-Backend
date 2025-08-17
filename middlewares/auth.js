880e2307034606d26b43aee2adaa2d829e3779c4
anurag8824 <ashutosh.0321sharma@gmail.com>
2025-08-18 00:53:36 +0530

    bug fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -26,7 +26,7 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   }
   console.log(token,"token");
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
